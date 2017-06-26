fb322497af0b9ed866fcdf3c270f8be28ea3d147
W. Ely Paysinger <paysinger@gmail.com>
2017-06-26 16:26:11 -0700

    working on front end 2

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,8 +1,21 @@
 """Views for the home page."""
 from django.shortcuts import render
-
+from imager_images.models import Photo
+import random
+# from imagersite.settings import MEDIA_ROOT
 
 def home_view(request):
     """View for home page."""
-    context = {'stuff': 'somestuff'}
+    images = Photo.objects.all().filter(published='PU')
+    image_url = None
+    if images:
+        random_img = random.choice(images)
+        image_url = random_img.image.url
+    else:
+        image_url = '/test/testing.png'
+
+    context = {
+        'stuff': 'somestuff',
+        'rando_imag': image_url
+    }
     return render(request, 'imagersite/home.html', context=context)
