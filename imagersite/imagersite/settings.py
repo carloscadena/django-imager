a53f422ff3351d8613282121e072964e533f6b37
W. Ely Paysinger <paysinger@gmail.com>
2017-07-13 18:34:27 -0700

    ansible

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -23,11 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# TEMPLATE_DEBUG = DEBUG
+DEBUG = os.environ.get('DEBUG', False)
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['ec2-34-209-26-144.us-west-2.compute.amazonaws.com', 'localhost']
-
+ALLOWED_HOSTS = [
+    'ec2-34-209-185-53.us-west-2.compute.amazonaws.com',
+    'ec2-34-211-255-112.us-west-2.compute.amazonaws.com',
+    'localhost'
+]
 
 # Application definition
 
@@ -128,9 +131,6 @@ DEFAULT_FROM_EMAIL = 'dangoldjangadange@gmail.com'
 DEFAULT_TO_EMAIL = 'paysinger@gmail.com'
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -141,52 +141,32 @@ USE_L10N = True
 
 USE_TZ = True
 
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(
+        AWS_STORAGE_BUCKET_NAME
+    )
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'imagersite.custom_storages.StaticStorage'
+    STATIC_URL = 'https://{}/{}/'.format(
+        AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION
+    )
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'imagersite.custom_storages.MediaStorage'
+    MEDIA_URL = 'htts://{}/{}/'.format(
+        AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION
+    )
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/var/www/static/',
-]
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+        '/var/www/static/',
+    ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
-
-
-# if not DEBUG:
-#     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#     AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(
-#         AWS_STORAGE_BUCKET_NAME
-#     )
-#
-#     STATICFILES_LOCATION = 'static'
-#     STATICFILES_STORAGE = 'imagersite.custom_storages.StaticStorage'
-#     STATIC_URL = 'https://{}/{}/'.format(
-#         AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION
-#     )
-#
-#     MEDIAFILES_LOCATION = 'media'
-#     DEFAULT_FILE_STORAGE = 'imagersite.custom_storages.MediaStorage'
-#     MEDIA_URL = 'htts://{}/{}/'.format(
-#         AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION
-#     )
-#
-# else:
-#     STATIC_URL = '/static/'
-#     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#
-#     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#     STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static'),
-#         '/var/www/static/',
-#     ]
-#
-#     MEDIA_URL = '/media/'
-#     MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
