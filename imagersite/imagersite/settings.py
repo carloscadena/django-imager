18a0f20b85fce541dc7de27bdab9d52499efaf89
W. Ely Paysinger <paysinger@gmail.com>
2017-07-15 11:30:45 -0700

    email debug mode added to send to console while in debug

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -132,7 +132,10 @@ ACCOUNT_ACTIVATION_DAYS = 7
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/profile'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
