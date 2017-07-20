a0abb4650a42776535a5f95d2374f895f086df04
Carlos Cadena <cs.cadena@gmail.com>
2017-07-20 15:27:56 -0700

    github and twitter logins working

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^profile/', include("imager_profile.urls")),
-    url(r'^images/', include("imager_images.urls"))
+    url(r'^images/', include("imager_images.urls")),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 
 ]
 
